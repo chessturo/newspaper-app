b0f136502f45e1fd1b599610d1db9104d4baf24f
Mitchell Levy <levymitchell0@gmail.com>
2019-01-20 12:38:40 -0800

    Adding Mongo and setting up examples for demo2

--- index.js
+++ index.js
@@ -11,20 +11,12 @@ app.use(helmet());
 let demo1 = express.Router();
 let demo2 = express.Router();
 
+//Demo1
 demo1.get('/', (req, res) => {
   res.send(`Yay, it's the homepage!`);
 });
 
-demo1.get('/:year/:month/:day', (req, res) => {
-  for (var param in req.params) {
-    if (req.params.hasOwnProperty(param)) {
-      if (!req.params[param].match(/^[0-9]+$/)) {
-        res.send(`Invalid URL`);
-        return;
-      }
-    }
-  }
-
+demo1.get('/:year([0-9]+)/:month([0-9]+)/:day([0-9]+)', (req, res) => {
   let year = req.params.year;
   let month = req.params.month;
   let day = req.params.day;
@@ -40,8 +32,16 @@ demo1.get('/:year/:month/:day', (req, res) => {
   }
 });
 
+demo1.get('*', (req, res) => {
+  res.status(404);
+  res.send(`This page does not exist`);
+});
+
+//Demo2
 demo2.get('/', (req, res) => {
-  res.send(`demo2`);
+  let header = fs.readFileSync('demo2/header.html');
+  let footer = fs.readFileSync('demo2/footer.html');
+  
 });
 
 app.use(vhost(`demo1.${hostname}`, demo1));
